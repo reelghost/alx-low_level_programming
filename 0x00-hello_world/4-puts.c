2225f56e71cbb4722ebb0f0b86d5ee759045647c
deadboneiha <reelghostz@gmail.com>
2022-12-08 16:43:27 -0800

    This is c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - This is the entry point
+ *
+ * Return to always check if its 0 which means success
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
