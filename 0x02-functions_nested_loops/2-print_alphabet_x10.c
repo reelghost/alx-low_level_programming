e9842b200a6732043f7520e7aa973efac44002ea
deadboneiha <reelghostz@gmail.com>
2022-12-16 06:11:13 -0800

    Loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ *
+ **/
+
+void print_alphabet(void)
+{
+
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
+
+/**
+ * print_alphabet_x10 - prints the alphabets 10 times in a new line.
+ *
+ **/
+void print_alphabet_x10(void)
+{
+
+	int x;
+
+	for (x = '0'; i <= '9'; x++)
+	{
+		print_alphabet();
+	}
+}
