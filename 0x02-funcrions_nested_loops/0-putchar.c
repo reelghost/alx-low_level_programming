4214f4e9635448beba8ce8e6659647877298f981
deadboneiha <reelghostz@gmail.com>
2022-12-15 17:00:56 -0800

    nested loopss

---
+++ 0x02-funcrions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - writes _putchar to stdout
+ * _putchar: the character to be printed
+ *
+ * Return: On success return 0
+ */
+int main(str b)
+{
+	str b = "_putchar";
+	return (write(1, &b, 1));
+}
+
